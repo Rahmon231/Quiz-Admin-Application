15b9266f153e88e4db6f38f8a0a0b6a936bf47d8
Akanbi Rahmon <Rahmon.Akanbi@outlook.com>
2022-05-15 21:27:48 +0100

    add new course dialog

--- app/src/main/java/com/lemzeeyyy/quizadminapplication/CourseActivity.java
+++ app/src/main/java/com/lemzeeyyy/quizadminapplication/CourseActivity.java
@@ -3,16 +3,32 @@ package com.lemzeeyyy.quizadminapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CourseActivity extends AppCompatActivity {
     private RecyclerView courseRecyclerView;
     private Button addCourse;
+    private List<String> courseList;
+    private FirebaseFirestore firestore;
+    private Dialog loadingDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +39,48 @@ public class CourseActivity extends AppCompatActivity {
         addCourse = findViewById(R.id.addCourseBtn);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Courses");
+        courseList = new ArrayList<>();
+        loadingDialog = new Dialog(CourseActivity.this);
+        loadingDialog.setContentView(R.layout.loadingprogressbar);
+        loadingDialog.setCancelable(false);
+        loadingDialog.getWindow().setBackgroundDrawableResource(R.drawable.progressbackground);
+        loadingDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        firestore = FirebaseFirestore.getInstance();
+        loadDate();
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        CourseAdapter adapter = new CourseAdapter(catList,CourseActivity.this);
-        courseRecyclerView.setAdapter(adapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        courseRecyclerView.setLayoutManager(layoutManager);
+
+
+    }
+    private void loadDate() {
+        loadingDialog.show();
+        courseList.clear();
+        firestore.collection("QUIZ").document("Categories")
+                .get().addOnCompleteListener(task -> {
+                    if(task.isSuccessful()){
+                        DocumentSnapshot doc = task.getResult();
+                        if(doc.exists()){
+                            long count = (long)doc.get("COUNT");
+                            for (int i = 1; i <= count; i++) {
+                                String courseName = doc.getString("CAT"+String.valueOf(i));
+                                courseList.add(courseName);
+                            }
+                            CourseAdapter adapter = new CourseAdapter(courseList,CourseActivity.this);
+                            courseRecyclerView.setAdapter(adapter);
+
+                        }else {
+                            Toast.makeText(CourseActivity.this, "no category document", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }else {
+                        Toast.makeText(CourseActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                    loadingDialog.dismiss();
+
+                });
     }
 }
--- app/src/main/java/com/lemzeeyyy/quizadminapplication/CourseAdapter.java
+++ app/src/main/java/com/lemzeeyyy/quizadminapplication/CourseAdapter.java
@@ -1,8 +1,11 @@
 package com.lemzeeyyy.quizadminapplication;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,22 +24,37 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.ViewHolder
     @NonNull
     @Override
     public CourseAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.course_item_layout,parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CourseAdapter.ViewHolder holder, int position) {
+        String title = catList.get(position);
+        holder.setData(title);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return catList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView courseName;
+        private ImageView deleteBtn;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            courseName = itemView.findViewById(R.id.courseNameID);
+            deleteBtn = itemView.findViewById(R.id.courseDeleteBtnID);
+        }
+
+        public void setData(String title) {
+            courseName.setText(title);
+            deleteBtn.setOnClickListener(view -> {
+
+            });
         }
     }
 }
